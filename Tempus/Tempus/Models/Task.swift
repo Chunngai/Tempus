bad3fba683ebe9e3626a1cb76b439677236f42e3
Chunngai <mikumikku3939@icloud.com>
2020-07-30 16:16:52 +0800

    simply Date.init(hour:minute:)

--- Tempus/Tempus/Models/Task.swift
+++ Tempus/Tempus/Models/Task.swift
@@ -57,15 +57,19 @@ extension Task {
 extension Date {
     // Creates a date with hour and min.
     init(hour: Int, minute: Int) {
-        let now = Date()
-
-        var dateComponents = DateComponents()
-        dateComponents.year = Calendar.current.component(.year, from: now)
-        dateComponents.month = Calendar.current.component(.month, from: now)
-        dateComponents.day = Calendar.current.component(.day, from: now)
+//        let now = Date()
+//
+//        var dateComponents = DateComponents()
+//        dateComponents.year = Calendar.current.component(.year, from: now)
+//        dateComponents.month = Calendar.current.component(.month, from: now)
+//        dateComponents.day = Calendar.current.component(.day, from: now)
+//        dateComponents.hour = hour
+//        dateComponents.minute = minute
+//        dateComponents.second = 0
+        
+        var dateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: Date())
         dateComponents.hour = hour
         dateComponents.minute = minute
-        dateComponents.second = 0
 	
         self = Calendar.current.date(from: dateComponents)!  // Timezone of the date generated is according to the system.
     }
